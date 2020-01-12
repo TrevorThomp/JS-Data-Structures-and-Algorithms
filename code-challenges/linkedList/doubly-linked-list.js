ff083c2148704bdc7b8a3b4f7e5e9216468e6476
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-12 14:16:14 -0800

    added stretch of a doubly linked list. Includes wont function with eslint so its missing counter variable for incrementing

---
+++ code-challenges/linkedList/doubly-linked-list.js
@@ -0,0 +1,53 @@
+'use strict';
+
+class Node {
+  constructor(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  includes(value) {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+}
+
+const list = new LinkedList();
+
+list.insert(1);
+list.insert(2);
+list.insert(3);
+
+let value = list.includes(5);
+
+console.log(value);
