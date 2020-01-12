69b14746b2717bd8eafaa0a9ac4568dc683f5b5c
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-12 11:17:20 -0800

    added test to ensure head is pointing to the first node

--- code-challenges/linkedList/__tests__/linked-list.test.js
+++ code-challenges/linkedList/__tests__/linked-list.test.js
@@ -15,5 +15,14 @@ describe('Linked List', () => {
     expect(linkedList.head.value).toEqual(1);
   });
 
+  it('Head will properly point to first node in linked list', () => {
+    let linkedList = new list.LinkedList();
+    linkedList.insert(1);
+    linkedList.insert(2);
+    linkedList.insert(3);
+    console.log(linkedList);
+    expect(linkedList.head.value).toEqual(3);
+  });
+
 
 })
