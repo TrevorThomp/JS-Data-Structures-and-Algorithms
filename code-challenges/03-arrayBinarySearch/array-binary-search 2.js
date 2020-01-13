905d582b392696632e7382d1d7bda8f8d02a383a
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-13 11:42:40 -0800

    renamed function and deleted extra README

---
+++ code-challenges/03-arrayBinarySearch/array-binary-search 2.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @function - Takes an array and value and finds the index of given key
+ * @param {Array} arr 
+ * @param {*} key 
+ * @returns value
+ */
+function binarySearch(arr,key) {
+  let left = 0;
+  let right = arr.length -1;
+
+  while (left <= right) {
+
+    let middle = Math.floor((left + right) / 2);
+
+    if (key === arr[middle]) {
+      return middle;
+    }
+      
+    if (key < arr[middle]) {
+      right = middle - 1;
+    }
+    
+    if (key > arr[middle]) {
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
+
+module.exports = binarySearch;
