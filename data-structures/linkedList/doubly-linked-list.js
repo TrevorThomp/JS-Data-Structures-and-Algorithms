ec5d25dc05a2a8a37adb300e05cb19fe3ab13ac0
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-13 09:28:37 -0800

    deleted node_modules

---
+++ data-structures/linkedList/doubly-linked-list.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @class
+ */
+class Node {
+  constructor(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+/**
+ * @class
+ */
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  includes(value) {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  }
+}
+
+module.exports = LinkedList;
