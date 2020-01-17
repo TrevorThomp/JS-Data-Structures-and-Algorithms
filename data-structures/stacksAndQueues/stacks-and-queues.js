4c78df3c2fc62f1473f063d2cfe3832b8bdb8f94
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-17 10:57:03 -0800

    added Stack class

--- data-structures/stacksAndQueues/stacks-and-queues.js
+++ data-structures/stacksAndQueues/stacks-and-queues.js
@@ -1,1 +1,14 @@
 'use strict';
+
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor(top) {
+    this.top = null;
+  }
+}
