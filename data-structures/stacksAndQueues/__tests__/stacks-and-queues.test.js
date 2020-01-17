0137c594aaeaa6da452c39917109905a61d1a9ff
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-17 13:55:11 -0800

    refactored stack testing suite

--- data-structures/stacksAndQueues/__tests__/stacks-and-queues.test.js
+++ data-structures/stacksAndQueues/__tests__/stacks-and-queues.test.js
@@ -5,40 +5,39 @@ const { Queue } = require('../stacks-and-queues');
 
 describe('Stack Tests', () => {
 
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
   it('Properly add a node to the top of the stack', () => {
-    let stack = new Stack();
     stack.push(1);
     expect(stack.top.value).toEqual(1);
   });
 
   it('Removes node from the stack', () => {
-    let stack = new Stack();
     stack.push(1);
     stack.push(2);
-    stack.pop()
-
+    stack.pop();
     expect(stack.top.value).toEqual(1);
   });
 
   it('Returns top node', () => {
-    let stack = new Stack();
     stack.push(1);
     stack.push(2);
-
     expect(stack.peek().value).toEqual(2);
   });
 
   it('Checks if stack is empty', () => {
-    let stack = new Stack();
-
     expect(stack.isEmpty()).toEqual(true);
   });
 
 });
 
-// describe('Queue Tests', () => {
+describe('Queue Tests', () => {
+
+  it
 
 
-
-
-// });
+});
