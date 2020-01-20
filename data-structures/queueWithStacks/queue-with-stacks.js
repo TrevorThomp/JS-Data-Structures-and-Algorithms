6f6b6611fff00df092f9b2f486fa76a5ac504736
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-20 12:49:37 -0800

    added dequeue method

--- data-structures/queueWithStacks/queue-with-stacks.js
+++ data-structures/queueWithStacks/queue-with-stacks.js
@@ -1,1 +1,99 @@
 'use strict';
+
+/**
+ * Node Class
+ * @class
+ */
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+/**
+ * Stack Class
+ * @class
+ */
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  /**
+   * Pushes node on to the top of the stack
+   * @param {*} value 
+   */
+  push(value) {
+    let newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+    this.length++;
+  }
+
+  /**
+   * Removes the top node on the stack
+   */
+  pop() {
+    let temp;
+
+    temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    this.length--;
+
+    return temp;
+  }
+
+  /**
+   * Return the top node on the stack
+   */
+  peek() {
+    return this.top;
+  }
+}
+
+class PseudoQueue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.stackOne = new Stack();
+    this.stackTwo = new Stack();
+  };
+
+
+
+  enqueue(value) {
+    let newNode = new Node(value);
+
+    if(!this.front) {
+      this.front = this.stackTwo;
+      this.rear = this.stackOne;
+    }
+    this.stackOne.push(newNode);
+  }
+
+  dequeue() {
+    if (this.stackTwo.length === 0) {
+      if(this.stackOne.length > 0) {
+        while (this.stackOne.length > 0) {
+          let popped = this.stackOne.pop();
+          this.stackTwo.push(popped);
+        }
+      }
+    }
+    return this.stackTwo.pop();
+  }
+
+}
+
+
+const queue = new PseudoQueue();
+
+queue.enqueue(1)
+queue.enqueue(1)
+queue.enqueue(1)
+queue.dequeue();
+
+console.log(queue)
