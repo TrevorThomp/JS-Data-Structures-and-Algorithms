983d7b9b1a0b5207921f31f5d3484cd4d876601b
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-27 12:59:15 -0800

    fizzbuzz functionality

---
+++ code-challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const {BinarySearchTree} = require('../../data-structures/tree/tree');
+
+
+
+function fizzBuzz(val) {
+  if (val % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (val % 5 === 0) {
+    return 'Buzz';
+  } else if (val % 3 === 0) {
+    return 'Fizz';
+  } else {
+    return val;
+  }
+}
+
+function fizzBuzzTreeBuzz(tree) {
+  let recursive = node => {
+    if (node.left) recursive(node.left);
+    node.val = fizzBuzz(node.val);
+    if (node.right) recursive(node.right);
+  };
+  recursive(tree.root);
+  return tree;
+}
+
+let BST = new BinarySearchTree(10);
+
+BST.add(15);
+BST.add(5);
+BST.add(6);
+BST.add(10);
+let newTree = fizzBuzzTreeBuzz(BST)
+
+console.log(newTree)
