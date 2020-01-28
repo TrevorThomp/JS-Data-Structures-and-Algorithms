0ad1b203afd3f2b631fc25b7141c24a9d1e548d8
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-28 13:37:52 -0800

    began scaffold

--- code-challenges/breadth-first/breadth-first.js
+++ code-challenges/breadth-first/breadth-first.js
@@ -7,11 +7,7 @@ const {Queue} = require('../../data-structures/stacksAndQueues/stacks-and-queues
 let BT = new BinarySearchTree(45);
 BT.add(75);
 BT.add(65);
-BT.add(55);
-BT.add(45);
 BT.add(35);
-BT.add(25);
-BT.add(15);
 
 function breadthFirst(tree) {
   let queue = new Queue();
