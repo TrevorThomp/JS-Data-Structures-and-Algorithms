0ad2da7506ba3dd50416bb388efc2fdc1288e60f
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-19 12:20:18 -0800

    added testing for quickSort

--- code-challenges/quickSort/__tests__/quickSort.test.js
+++ code-challenges/quickSort/__tests__/quickSort.test.js
@@ -1,20 +1,15 @@
 'use strict';
 
-const mergeSort = require('../quickSort');
+const quickSort = require('../quickSort');
 
 describe('Quick', () => {
 
   it('Returns error when given no array', () => {
-    expect(mergeSort()).toBe('No Array Given');
+    expect(quickSort()).toBe('No Array Given');
   });
 
   it('Properly sorts array', () => {
     const array = [8,4,23,15,42,16];
-    expect(mergeSort(array)).toEqual([4,8,15,16,23,42]);
-  });
-
-  it('Returns error when given empty array', () => {
-    const array = [];
-    expect(mergeSort(array)).toBe('Empty Array');
+    expect(quickSort(array)).toEqual([4,8,15,16,23,42]);
   });
 });
--- code-challenges/quickSort/quickSort.js
+++ code-challenges/quickSort/quickSort.js
@@ -1,2 +1,27 @@
 'use strict';
 
+function QuickSort(array) {
+  if (!array) return 'No Array Given';
+
+  if (array.length <= 1) {
+    return array;
+  } else {
+    const left = [];
+    const right = [];
+    const newArray = [];
+    let pivot = array.pop();
+    let length = array.length;
+
+    for (let i = 0; i < length; i++) {
+      if (array[i] <= pivot) {
+        left.push(array[i])
+      } else {
+        right.push(array[i])
+      }
+    }
+
+    return newArray.concat(QuickSort(left), pivot, QuickSort(right))
+  }
+}
+
+module.exports = QuickSort;
