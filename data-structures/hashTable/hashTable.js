fe0cd8bb874d385fb3519fdf085bd772fb97f3e7
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-21 11:18:43 -0800

    added get functionality

--- data-structures/hashTable/hashTable.js
+++ data-structures/hashTable/hashTable.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -12,74 +11,94 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
   add(value) {
+    if(!value) throw new Error('Invalid value');
+
     const node = new Node(value);
-    if ( ! this.head ) {
+
+    if (!this.head) {
       this.head = node;
       return;
     }
 
     let current = this.head;
-    while( current.next ) {
+
+    while(current.next) {
       current = current.next;
     }
+
     current.next = node;
+  }
+
+  getKey(key) {
+    if (!this.head) return;
+
+    let current = this.head;
+
+    while (current) {
+      if (current.value[0] === key) return current.value[1]; 
+      current = current.next;
+    }
+
+    return;
   }
 
   values() {
     let values = [];
     let current = this.head;
+
     while(current) {
       values.push(current.value);
       current = current.next;
     }
+
     return values;
-  }
-
-  getKey(key) {
-    if (!key) return 'No Key Provided';
-
-    let current = this.head;
-
-    while (current) {
-      if (current.value[0] === key) return current.value[1]
-      current = current.next;
-    }
-    return;
   }
 }
 
 class Hashmap {
-
   constructor(size) {
-    this.size = size;
-    this.map = new Array(size); //.fill();
+    this.size = size || 5,
+    this.buckets = new Array(this.size);
   }
 
   hash(key) {
     return key.split('').reduce((p, n) => {
       return p + n.charCodeAt(0);
     },0) * 599 % this.size;
-    
   }
 
-  add(key,value) {
-    let hash = this.hash(key);
-    if(! this.map[hash] ) { this.map[hash] = new LinkedList(); }
+  add(key, value) {
+    if (!key) throw new Error('Invalid key provided');
+    if (this.contains(key)) return 'Key already being used';
 
-    // We used an object here, but this could be anything...
-    let entry = { [key]: value };
-    //this.map[has] is an instance of the LL that you can add an etry to
-    this.map[hash].add( entry );
+    let index = this.hash(key);
+
+    if (!this.buckets[index])  { this.buckets[index] = new LinkedList(); }
+
+    this.buckets[index].add([key, value]);
+  
   }
 
-  /**
-   * Find a key in the hashmap and return its value
-   * @param key
-   * @return {string}
-   */
   get(key) {
+    if (!key) throw new Error('Invalid key provided');
 
+    let index = this.hash(key);
+
+    if (!this.buckets[index])  { return null; }
+
+    return this.buckets[index].getKey(key);
+  }
+
+  contains(key) {
+    if (!key) throw new Error('Invalid key provided');
+
+    let index = this.hash(key);
+
+    if (!this.buckets[index])  { return null; }
+
+    return this.buckets[index].getWithKey(key) ? true : false;
   }
 }
 
@@ -99,6 +118,6 @@ myhash.add('Khalil','Student');
 myhash.add('Michael','Student');
 myhash.add('Ovi','Student');
 
-myhash.map.forEach( (data,i) => {
-  console.log(i, data && data.values());
-});
+
+let get = myhash.get('Timea');
+console.log(get)
