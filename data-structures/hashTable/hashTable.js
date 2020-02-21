069337c860d343c5118eaad69ca4e264522d342d
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-21 12:44:58 -0800

    added contains method and exported classes

--- data-structures/hashTable/hashTable.js
+++ data-structures/hashTable/hashTable.js
@@ -102,22 +102,4 @@ class Hashmap {
   }
 }
 
-let myhash = new Hashmap(1024);
-myhash.add('John','Boss');
-myhash.add('Cathy','The Real Boss');
-myhash.add('Zach','Kid 1');
-myhash.add('Allie','Kid 2');
-myhash.add('Rosie','Dog');
-myhash.add('Cat','TA');
-myhash.add('Justin','Student');
-myhash.add('Jason','Student');
-myhash.add('Ben','Student');
-myhash.add('Timea','Student');
-myhash.add('Jen','Student');
-myhash.add('Khalil','Student');
-myhash.add('Michael','Student');
-myhash.add('Ovi','Student');
-
-
-let get = myhash.get('Timea');
-console.log(get)
+module.exports = {LinkedList, Hashmap};
