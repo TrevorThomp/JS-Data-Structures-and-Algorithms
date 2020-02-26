445d9a8563a4962820a4cd0cad9701da74fa961f
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-26 13:40:20 -0800

    removed console logs

--- code-challenges/leftJoin/leftJoin.js
+++ code-challenges/leftJoin/leftJoin.js
@@ -2,10 +2,9 @@
 
 function leftJoin(left, right){
   const joinedData = [];
-  if (typeof(left) !== 'object' && typeof(right) !== 'object') return 'Not Valid Argument'
+  if (typeof(left) !== 'object' && typeof(right) !== 'object') return 'Not Valid Argument';
 
   left.buckets.forEach(bucket => {
-    //console.log(bucket.head.value);
     let key = bucket.head.value[0];
     right.contains(key) ? joinedData.push([...bucket.head.value, right.get(key)]) : joinedData.push(bucket.head.value);    
   });
