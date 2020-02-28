e5c4fb686d9b6d6e776e4d420f7344d12ed6a6d1
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-28 10:46:23 -0800

    added addNode method

--- data-structures/graph/graph.js
+++ data-structures/graph/graph.js
@@ -1,17 +1,59 @@
 'use strict';
 
+const util = require('util');
+
 /**
  * @class
  */
-class Node {
-  constructor(value ,next) {
+class Vertex {
+  constructor(value){
     this.value = value;
-    this.next = next;
   }
 }
 
-class Graph {
-  constructor() {
-    
+/**
+ * @class
+ */
+class Edge {
+  constructor(vertex, weight){
+    this.vertex = vertex;
+    this.weight = weight || 0;
   }
 }
+
+/**
+ * @class
+ */
+class Graph{
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(value){
+  
+  }
+  //directed edge add
+  addDirectedEdge( startVertex, endVertex, weight=0) {
+
+    //make sure vertices exist
+
+    //get/retrieve what's currently in my map object at the specified vertex - startVertex.
+    //for that startVertex, push into the corresponding array (array is the value in the key:value pair)
+
+
+  }
+  getNeighbors(value) {
+    //for given vertex argument, return all of its neighbors
+
+  }
+
+  getVertices(){
+  //returns all of the vertices in the graph data structure
+  }
+
+  size(){
+  //count of number of vertices in the graph
+  }
+}
+
+module.export = Graph;
