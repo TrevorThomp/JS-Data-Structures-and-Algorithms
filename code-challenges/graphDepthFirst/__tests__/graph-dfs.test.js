e1018686d9cb8e3c622a0a88d63ebbef57253078
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-03-04 13:09:55 -0800

    added depthfirst method

--- code-challenges/graphDepthFirst/__tests__/graph-dfs.test.js
+++ code-challenges/graphDepthFirst/__tests__/graph-dfs.test.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const Stack = require('../../../data-structures/stacksAndQueues/stacks-and-queues');
+
+module.exports = (graph) => {
+  let stack = new Stack();
+  let output = [];
+  let visited = [];
+
+  let startVertex = graph.adjacencyList.keys().next().value;
+  stack.push(startVertex);
+  visited.push(startVertex);
+  output.push(startVertex.value);
+
+  while(stack.peek()){
+
+    let neighborsArr = graph.getNeighbors(startVertex);
+    for(let neighbor of neighborsArr){
+      if(!visited.includes(neighbor)){
+        stack.push(neighbor);
+        visited.push(neighbor);
+        output.push(neighbor);
+        startVertex = neighbor;
+        break;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+};
